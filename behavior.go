0d8c6bced7364e08e672c81463fbca28a914d370
Chris Hess <christhegoalie@gmail.com>
2024-08-30 16:17:10 -0500

    UPD: updated warclaw caches to reset daily

--- behavior.go
+++ behavior.go
@@ -5,7 +5,7 @@ type Behavior int
 const (
 	Behavior_Default Behavior = 0 //always visible
 	//Behavior_Reset_On_Map_Change Behavior  = 1
-	Behavior_Reaper_On_Daily_Reset     Behavior = 2
+	Behavior_Reset_On_Daily_Reset      Behavior = 2
 	Behavior_Visible_Before_Activation Behavior = 3 //disapear forever on interact
 	Behavior_Reapear_After_Timer       Behavior = 4 //use resetLength as timer
 	//Behavior_Reapear_After_Reset Behavior = 5
--- go.mod
+++ go.mod
@@ -4,6 +4,8 @@ go 1.21.5
 
 require github.com/otiai10/copy v1.14.0 // direct
 
+require github.com/google/uuid v1.6.0
+
 require (
 	golang.org/x/sync v0.8.0 // indirect
 	golang.org/x/sys v0.24.0 // indirect
