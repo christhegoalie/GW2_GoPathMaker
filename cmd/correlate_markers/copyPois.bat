9e6bc636ead307de6e4e5ba9874eca6c003b8a1c
Chris Hess <christhegoalie@gmail.com>
2024-09-01 21:12:28 -0500

    UPD: new markers UPD: rebuilt trails ADD: correlation data ADD: added scripts to copy poi data for correlation calcs

---
+++ cmd/correlate_markers/copyPois.bat
@@ -0,0 +1,2 @@
+copy "../../ShellshotMarkerPack/maps/Janthir Syntri/DigSpots/WarclawCache.poi" .
+copy "../../ShellshotMarkerPack/maps/LowlandShore/DigSpots/WarclawCache.poi" .
---
+++ cmd/correlate_markers/copyPois.sh
@@ -0,0 +1,4 @@
+#! /bin/bash
+
+cp "../../ShellshotMarkerPack/maps/Janthir Syntri/DigSpots/WarclawCache.poi" .
+cp "../../ShellshotMarkerPack/maps/LowlandShore/DigSpots/WarclawCache.poi" .
